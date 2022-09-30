18a692e51cb0c3852014b1c4b586fa272d829363
atabekdemurtaza <mychannel21war@gmail.com>
2022-09-30 22:00:18 +0500

    30.09.2022 22:00

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from pages.views import EntryView
+from pages.views import EntryView, EntryDetail
 
 urlpatterns = [
-    path('', EntryView.as_view())
+    path('', EntryView.as_view()),
+    path('<int:pk>/', EntryDetail.as_view())
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,4 +6,9 @@ class EntryView(generic.ListView):
     template_name = 'pages/page_list.html'
     queryset = Entry.objects.all()
     context_object_name = 'pages' #objects
+
+class EntryDetail(generic.DetailView):
+
+    template_name = 'pages/page_detail.html'
+    queryset = Entry.objects.all()
     
